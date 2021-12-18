6e773ccc666aa59489ca41f1cda737d9b2d0bc6e
Atrizy <evankeeping21@gmail.com>
2021-12-18 14:50:36 -0330

    Added the tweets and made the toggle button work

---
+++ src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="app">
+    <div class="tweet" v-if="clicked === true">
+      <user-tweets
+        v-for="tweet in tweets"
+        :key="tweet['author']"
+        :author="tweet['author']"
+        :content="tweet['content']"
+        :timestamp="tweet['timestamp']"
+      >
+      </user-tweets>
+    </div>
+    <div class="tweets" v-else>
+      <user-tweets
+        v-for="tweet in tweets"
+        :key="tweet['author']"
+        :author="tweet['author']"
+        :content="tweet['content']"
+        :timestamp="tweet['timestamp']"
+      >
+      </user-tweets>
+    </div>
+    <button @click="tweet_switch">Change layout</button>
+  </div>
+</template>
+
+<script>
+import UserTweets from "./components/UserTweets.vue";
+
+export default {
+  name: "App",
+  methods: {
+    //This bit right here is what makes the tweet switches toggle
+    tweet_switch() {
+      if (this.clicked === false) {
+        this.clicked = true;
+      } else {
+        this.clicked = false;
+      }
+    },
+  },
+  data() {
+    return {
+      clicked: false,
+      tweets: [
+        {
+          author: "@OliviaWidle",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2021",
+        },
+        {
+          author: "@Memer",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2020",
+        },
+        {
+          author: "@Memer",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2020",
+        },
+        {
+          author: "@Memer",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2020",
+        },
+        {
+          author: "@Memer",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2020",
+        },
+        {
+          author: "@Memer",
+          content:
+            "In a thousand years archeologists will dig up tanning beds and think we used them to fry people as punishment.",
+          timestamp: "5/14/2020",
+        },
+      ],
+    };
+  },
+  components: {
+    UserTweets,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  margin-top: 15px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: black;
+}
+
+.tweets {
+  display: grid;
+  grid-auto-rows: auto;
+}
+
+.tweet {
+  display: grid;
+  grid-template-columns: 200px 200px;
+  gap: 30px;
+  justify-self: center;
+  align-self: center;
+  text-align: center;
+  padding-top: 10px;
+  margin-left: 10px;
+}
+
+.tweet h6 {
+  padding-bottom: 8px;
+}
+
+.tweet p {
+  width: 200px;
+}
+
+.tweet h5 {
+  padding-top: 10px;
+  padding-bottom: 0px;
+}
+
+button {
+  position: fixed;
+  justify-self: center;
+  max-width: 100px;
+}
+</style>
---
+++ src/components/UserTweets.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>  
+        <h6>{{ author }}</h6>
+        <p>{{ content }}</p>
+        <h5>{{ timestamp }}</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "user-tweets",
+        props: {
+            author: String,
+            content: String,
+            timestamp: String
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
